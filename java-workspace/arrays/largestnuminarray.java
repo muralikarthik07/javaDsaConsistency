dedba56e027d0a42f349a587c6b4437a0d5eddd9
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-06 14:08:15 +0530

    largestnumarray.java

--- java-workspace/arrays/largestnuminarray.java
+++ java-workspace/arrays/largestnuminarray.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class largestnuminarray {
+
+    public static int largestvalue(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0; i<numbers.length; i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
     public static void main(String [] args){
-        
+        int numbers[] = {20,30,10,50,40};
+        System.out.println("largest value is:" + largestvalue(numbers));
     }
 }
