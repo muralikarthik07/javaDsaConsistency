caa0bddbe4a683e03a221a8a99002acf88e69a25
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-08 22:52:51 +0530

    kandanes

--- java-workspace/arrays2/maxSubArrayKadanes.java
+++ java-workspace/arrays2/maxSubArrayKadanes.java
@@ -1,7 +1,22 @@
 package arrays2;
-
+import java.util.*;
 public class maxSubArrayKadanes {
+    public static void kadanes(int numbers[]){
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for(int i=0; i<numbers.length; i++){
+            currSum = currSum + numbers[i];
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+
+        maxSum = Math.max(maxSum, currSum);
+        System.out.println("MaxSum is: " +maxSum);
+    }
     public static void main(String [] args){
-        
+        int numbers[] = {-2,1,-3,4,-1,2,1,-5,4};
+        kadanes(numbers);
     }
 }
