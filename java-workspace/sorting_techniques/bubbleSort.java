4c17f94101415eaa23748a1307e5b9e07e15ee74
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-12 13:27:38 +0530

    bubble sort

---
+++ java-workspace/sorting_techniques/bubbleSort.java
@@ -0,0 +1,27 @@
+package sorting_techniques;
+
+public class bubbleSort {
+    public static void bubbleSortAlgorithm(int arr[]){
+        for(int turn=0;turn<arr.length;turn++){
+            for(int j = 0;j<arr.length-1-turn; j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    
+    public static void main(String [] args){
+        int arr[] = {5, 4, 1, 3, 2};
+        bubbleSortAlgorithm(arr);
+        printArr(arr);
+    }
+}
