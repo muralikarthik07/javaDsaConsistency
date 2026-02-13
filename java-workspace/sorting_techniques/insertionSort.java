6cbb79bb7e8b9a790dc5a9973caf7baf45a18a5f
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-13 22:30:18 +0530

    insertion sort

---
+++ java-workspace/sorting_techniques/insertionSort.java
@@ -0,0 +1,30 @@
+package sorting_techniques;
+
+import java.util.*;
+
+public class insertionSort {
+
+    public static void insertionSorting(int arr[]){
+        for(int i=1; i<arr.length;i++){
+            int curr = i;
+            int prev = i-1;
+
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;  
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void printarray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {5, 4, 1, 3, 2};
+        insertionSorting(arr);
+        printarray(arr);
+    }
+}
