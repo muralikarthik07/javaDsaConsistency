7d29e170608173ac9df6fbecd4e433c9ddedbae4
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-13 23:07:39 +0530

    inbuilt sort

---
+++ java-workspace/sorting_techniques/inbuiltSort.java
@@ -0,0 +1,15 @@
+package sorting_techniques;
+import java.util.*;
+public class inbuiltSort {
+
+    public static void printArray(int arr[]){
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,7,4,9,1,0,4};
+        printArray(arr);
+    }
+}
