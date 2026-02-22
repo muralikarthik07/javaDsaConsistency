bda32950f63795eb4354be9306ad9e383be01788
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-22 11:26:53 +0530

    counting sort

---
+++ java-workspace/sorting_techniques/countingSort.java
@@ -0,0 +1,13 @@
+public class countingSort{
+
+    public static void countingSort(int arr[]){
+        
+    }
+    public static void main(String [] args){
+        int arr[] = {4,2,2,8,3,3,1};
+        countingSort(arr);
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
