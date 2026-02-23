e32e650a35d521924defb02e673f6389773ca1fe
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-23 12:01:37 +0530

    character at

---
+++ java-workspace/Strings/characterAt.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class characterAt {
+    public static void main(String[] args){
+        String name  = "Murali karthik";
+        for(int i=0; i<name.length();i++){
+            System.out.println(name.charAt(i)+ " " + "is at index" + i);
+        }
+    }
+}
