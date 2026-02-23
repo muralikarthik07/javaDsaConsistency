bf4e1c6c04e3926084848463342f5f73dab39b99
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-23 11:46:39 +0530

    length in strings

---
+++ java-workspace/Strings/StringsLength.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringsLength {
+    public static void main(String[] args){
+        String name = "Murali karthik";
+        System.out.println(name.length());
+    }
+    
+}
