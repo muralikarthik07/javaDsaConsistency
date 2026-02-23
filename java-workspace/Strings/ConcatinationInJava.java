ce998f6afd30a6904bc4d4843b60585fb8eaed82
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-23 11:54:35 +0530

    concat

---
+++ java-workspace/Strings/ConcatinationInJava.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class ConcatinationInJava {
+    public static void main(String [] args){
+        String firstName = "Murali";
+        String lastName = "Beeram";
+        String fulName = firstName+" "+ lastName;
+        System.out.println(fulName);
+    }
+}
