2f31464020f63f46d2e33368f386e9927ffff627
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-23 11:42:13 +0530

    Strings

---
+++ java-workspace/Strings/Strings.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Strings {
+    public static void main(String [] args){
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+        
+    }
+}
---
+++ java-workspace/Strings/inputInStrings.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.*;
+public class inputInStrings {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
