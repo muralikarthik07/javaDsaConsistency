a6c0b9799bfe00a73bb1c5e9c52235bdc16da0b4
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-24 19:21:16 +0530

    programs

---
+++ java-workspace/TCS nqt/checkPrime.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class checkPrime {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+}
