314c9a7850a61c3c506fb1791fbb9044a9eb351a
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-24 20:35:26 +0530

    prime number

--- java-workspace/Strings/eachFirstletterUpperCase.java
+++ java-workspace/Strings/eachFirstletterUpperCase.java
@@ -1,6 +1,7 @@
 package Strings;
 
 public class eachFirstletterUpperCase {
+    
     public static void main(String[] args){
         String str = "hii, i am karthik";
     }
--- java-workspace/TCS nqt/checkPrime.java
+++ java-workspace/TCS nqt/checkPrime.java
@@ -1,8 +1,27 @@
 import java.util.*;
 public class checkPrime {
+
+    public static boolean isPrime(int n){
+
+        if(n <= 1 ){
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if(isPrime(n)){
+            System.out.println("it is a prime number");
+        } else {
+            System.out.println("not a prime number");
+        }
         
+        sc.close();
     }
 }
