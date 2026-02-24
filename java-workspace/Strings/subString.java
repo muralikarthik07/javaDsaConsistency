f3188b9ef62756497bfb42251aec7acd434e781a
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-24 11:48:38 +0530

    Sub string

---
+++ java-workspace/Strings/subString.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class subString {
+    public static void main(String[] args){
+        String name = "murali karthik";
+        System.out.println(name.substring(0, 5));
+    }
+}
