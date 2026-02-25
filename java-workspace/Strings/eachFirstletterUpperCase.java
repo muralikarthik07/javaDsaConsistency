c3870fce16caf39bc670c35b4f54263d37da269b
murali karthik <muralikarthikbeeram07@gmail.com>
2026-02-25 15:25:15 +0530

    eachFirstLetterUpperCase

--- java-workspace/Strings/eachFirstletterUpperCase.java
+++ java-workspace/Strings/eachFirstletterUpperCase.java
@@ -2,7 +2,24 @@ package Strings;
 
 public class eachFirstletterUpperCase {
     
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        
+        for(int i = 1; i<str.length(); i++){
+            if(str.charAt(i)== ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args){
         String str = "hii, i am karthik";
+        System.out.println(toUpperCase(str));
+
     }
 }
--- java-workspace/Strings/subString.java
+++ java-workspace/Strings/subString.java
@@ -1,6 +1,7 @@
 package Strings;
 
 public class subString {
+    
     public static void main(String[] args){
         String name = "murali karthik";
         System.out.println(name.substring(0, 5));
